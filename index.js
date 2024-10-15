04e808c7ee3742b0c43752da951b98c960613268
2how <24274717+2how@users.noreply.github.com>
2024-10-15 20:22:27 +0200

    solve issue in setExercise

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const setExercise = () => {
   const set = new Set([1,2,3,3,4,4,4,5,5,5]);
   const finalSet = new Set();
 
-  set.array.forEach(element => {
+  set.forEach(element => {
     if (!finalSet.has(element)) {
       finalSet.add(element);
     }
