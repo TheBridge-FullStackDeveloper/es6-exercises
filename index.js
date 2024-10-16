0e00dbeb899522e2768234c0d3961c2cca25a953
Girgetto <girgetto@users.noreply.github.com>
2024-10-16 20:19:53 +0200

    Refactor code and comment out unused function implementations

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const rectangleArea = /* Añade tu código aquí */;
+// const rectangleArea = /* Añade tu código aquí */;
 
-const greet = (nombre) => /* Añade tu código aquí */;
+// const greet = (nombre) => /* Añade tu código aquí */;
 
-const mergeArray = (arr1, arr2) => /* Añade tu código aquí */;
+// const mergeArray = (arr1, arr2) => /* Añade tu código aquí */;
 
-const ducplicateAndSum = (a, b, /* Añade tu código aquí */) => {
-  const suma = a + b;
-  return numeros.map((numero) => numero + suma);
-};
+// const ducplicateAndSum = (a, b, /* Añade tu código aquí */) => {
+//   const suma = a + b;
+//   return numeros.map((numero) => numero + suma);
+// };
 
-const setExercise = () => {
-  const miSet = new Set([1, 2, 3, 3, 4, 5]);
-  /* Añade tu código aquí */
-};
+// const setExercise = () => {
+//   const miSet = new Set([1, 2, 3, 3, 4, 5]);
+//   /* Añade tu código aquí */
+// };
 
-const miMapa = new Map();
-miMapa.set("nombre", "Juan");
-miMapa.set("edad", 30);
-miMapa.set("profesion", "Desarrollador");
+// const miMapa = new Map();
+// miMapa.set("nombre", "Juan");
+// miMapa.set("edad", 30);
+// miMapa.set("profesion", "Desarrollador");
 
-const mapExercise = () => {
-  /* Añade tu código aquí */
-};
+// const mapExercise = () => {
+//   /* Añade tu código aquí */
+// };
 
-const countRepeated = (string) => {
-  /* Añade tu código aquí */
-};
+// const countRepeated = (string) => {
+//   /* Añade tu código aquí */
+// };
